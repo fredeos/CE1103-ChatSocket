7c1e94c2d9e91f875e1aca1472142b06f7f48fed
Frederick <134977839+fredeos@users.noreply.github.com>
2023-08-19 11:46:48 -0600

    Added GUI and Server/Client files

---
+++ Server_Client.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.*;
+
+class Server{
+    public static void main(String[] args){
+
+    }
+}
+class Client{
+    public static void main(String[] args){
+
+    }
+}
