a6b64b4895560d8ee344c533df447f78ed2c2bf6
Frederick <134977839+fredeos@users.noreply.github.com>
2023-08-23 11:58:40 -0600

    Revert Server.java

--- src/dev/Server.java
+++ src/dev/Server.java
@@ -1,57 +1,24 @@
 package src.dev;
-//Libraries needed for the class
+
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
-public class ChatServer {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+public class Server{
+    /*Boots the server to listen to the designed port and IP*/
+    public static void startup(){
+        String IP = "127.0.0.1";
+        int PORT =  3000;
+        try{
+            InetAddress address = InetAddress.getByName(IP);
+            // Line 18: binds the desired string IP to the network as a host
+            ServerSocket serverSocket = new ServerSocket(PORT, 20, address);
+            System.out.println("Listening on: " + PORT + " at " + IP);
 
-    public void start(int port) throws IOException {//Iniatilizing the server using sockets
-        serverSocket = new ServerSocket(port);
-        System.out.println("Servidor iniciado en el puerto " + port);
-
-        clientSocket = serverSocket.accept();
-        System.out.println("Cliente conectado: " + clientSocket.getInetAddress());
-
-        // messages of entry and exit
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
+        catch(IOException error){
+            System.out.println("Got error");
+            error.getStackTrace();
+        }
     }
-
-    public void sendMessage(String message) {// method to send messeges to client
-        out.println(message);
-    }
-
-    public String receiveMessage() throws IOException {// method to receive messeges from client
-        return in.readLine();
-    }
-
-    public void stop() throws IOException {// method to stop the server if it requiered
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
-        System.out.println("Servidor detenido");
-    }
-
-    """public static void main(String[] args) { // this method is an example of the way to use the class
-        ChatServer server = new ChatServer();
-        int port = 12345; // Port that the server will listen
-        try {
-            server.start(port);
-
-            // This is just an example of how to use the class
-            server.sendMessage("¡Hola cliente!");
-            String receivedMessage = server.receiveMessage();
-            System.out.println("Mensaje del cliente: " + receivedMessage);
-
-            server.stop();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }"""
-    }
+}
 
