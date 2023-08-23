cde7ebf7e2d708e6a650b124920c0460fdd9de47
Frederick <134977839+fredeos@users.noreply.github.com>
2023-08-23 12:06:59 -0600

    Update Client.java
    Added new methods for sending and receiving messages and new parameters for calling the methods

--- src/dev/Client.java
+++ src/dev/Client.java
@@ -1,22 +1,45 @@
 package src.dev;
 
+import src.dev.Server;
 import java.io.*;
 import java.net.*;
+import java.util.zip.InflaterInputStream;
+import javax.imageio.IIOException;
 
+/*The Client.java(& Client.class) is the separate file for setting up the client socket */
 public class Client{
-    /*Initiates the socket to connect to the server*/
-    public static void connect(){
+    public static String receivedmsg;
+
+    private static Socket clientSocket;
+    private static BufferedReader in;
+    private static PrintWriter out;
+
+/*Initiates the socket to connect to the server
+ * parameters(String argIP, int argPort)
+*/
+    public static void connect(String argIP, int argPort) throws IOException{
     // These are the port and IP addres to which the client connects
-        String SERVER_IP = "127.0.0.1";
-        int SERVER_PORT =  3000;
+        String SERVER_IP = argIP;
+        int SERVER_PORT =  argPort;
         try{
-            Socket clientSocket = new Socket(SERVER_IP, SERVER_PORT);
+            clientSocket = new Socket(SERVER_IP, SERVER_PORT);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
             System.out.println("Connecting to server at: " + "PORT=" + SERVER_PORT + "; IP=" + SERVER_IP);
-        }
-    //El catch obtiene los errores cuando no hay conexion con los puertos o con la IP.
-        catch (IOException error){
-            System.out.println("Got error");
+        } catch (IOException error){ //El catch obtiene los errores cuando no hay conexion con los puertos o con la IP.
+            System.out.println("No server found at: " + SERVER_IP+", "+ SERVER_PORT);
             error.getStackTrace();
+        }
+    }
+    public static void sendmessage(String message) throws IOException{
+        out.println(message);
+    }
+
+    public static void recievemsg() throws IOException{
+        if (!(in.readLine().trim()).isEmpty()){
+            receivedmsg = in.readLine();
+        } else {
+            receivedmsg = null;
         }
     }
 }
