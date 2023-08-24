4b3e16e1835dcdfd182f597a80d7fd75cb969126
Frederick <134977839+fredeos@users.noreply.github.com>
2023-08-24 11:38:26 -0600

    Update Server.java
    - Removed unused imports
    - Added the necessary comments and documentation

--- src/dev/Server.java
+++ src/dev/Server.java
@@ -1,54 +1,58 @@
 package src.dev;
 
-import src.dev.Client;
-import src.dev.logicalgi;
-
 import java.io.*;
 import java.net.*;
-import java.util.zip.InflaterInputStream;
-
+/*Class for Server creation and manipulation*/
 public class Server {
-    private static String receivedmsg;
+    private static String receivedmsg; // String variable for storing a message
     private static ServerSocket serverSocket;
     private static Socket clientSocket;
-    private static PrintWriter out;
-    private static BufferedReader in;
+    private static PrintWriter out; //PrintWriter for sending messgae
+    private static BufferedReader in; //Reader for receiving and decoding a message
 
+    /* Boots up the Server Sokcet and starts the queue for receiving clients
+    *@param argIP a string containing the IP to boot the server on
+    *@param argPort a integer value containing the port for the server
+    */
     public static void startup(String argIP, int argPort) throws IOException { //Iniatilizing the server using sockets
-        //Se asigna una variable para convertir el IP del server
-        InetAddress address = InetAddress.getByName(argIP);
-        serverSocket = new ServerSocket(argPort,1,address);
+        InetAddress address = InetAddress.getByName(argIP); //This converts the received IP address to an Inet address usable for creating the ServerSocket
+        serverSocket = new ServerSocket(argPort,1,address); //Creates the server socket
         System.out.println("Server started at: " + argPort + ", " + argIP);
 
-        //Se inicia un thread separado para esperar la conneccion de un socket cliente
+        //A new thread is created to wait on standby until a client connects to the sever
         Thread clientThread = new Thread(()->{
             try {
-                serverSocket.setSoTimeout(10000);
-                clientSocket = serverSocket.accept();
+                serverSocket.setSoTimeout(10000); //Sets a 10 second wait until timeout(the method receives miliseconds)
+                clientSocket = serverSocket.accept(); // Creates a client socket based on the connected client
                 System.out.println("Client connected: " + clientSocket.getInetAddress());
             // Messages of entry and exit
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new PrintWriter(clientSocket.getOutputStream(), true); //Writes OUT messages to the client
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Receives IN messages from the client
             } catch (IOException error){
                 error.getStackTrace();
             }
         });
-        clientThread.start();
+        clientThread.start(); //Starts the thread
     }
-    public static void sendMessage(String message) {// method to send messages to client
+
+    /* Sends a a message to the BufferedReader of the client
+    *@param message a string text that the server wants to send
+    */
+    public static void sendMessage(String message) {// Method to send messages to client
         out.println(message);
         System.out.println("You(host) sent: "+ message);
     }
 
+    /* Checks for a received message and updates the log on the Client side
+    */
     public static void receiveMessage() throws IOException {// method to receive messages from client
-        receivedmsg = in.readLine().trim();
+        receivedmsg = in.readLine().trim(); //Receives the decoded message and trims it to remove any blank spaces.
         System.out.println("Client sent: " + receivedmsg);
-        logicalgi.updatelog(receivedmsg, true);
+        chatGI.updatelog(receivedmsg, true); //Moves the message over to the client message display(log)
     }
 
-    //public static void getmessage(){}
-
-    public static void stop() throws IOException {// method to stop the server if it required
+    /*Stops the server socket and the connection with the client*/
+    public static void stop() throws IOException {
         in.close();
         out.close();
         clientSocket.close();
