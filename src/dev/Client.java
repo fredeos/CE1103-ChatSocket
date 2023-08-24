e913cc8ff7b4916a2d85506cf79a9855b6bf2d54
Frederick <134977839+fredeos@users.noreply.github.com>
2023-08-24 11:51:53 -0600

    Update Client.java
    - Removed unnecessary imports
    - Added the necessary comments and documentation.

--- src/dev/Client.java
+++ src/dev/Client.java
@@ -1,45 +1,48 @@
 package src.dev;
 
-import src.dev.Server;
-import src.devlogigalgi;
-
 import java.io.*;
 import java.net.*;
-import java.util.zip.InflaterInputStream;
-import javax.imageio.IIOException;
 
 /*The Client.java(& Client.class) is the separate file for setting up the client socket */
 public class Client{
-    private static String receivedmsg;
+    private static String receivedmsg; //String variable for received messages
     private static Socket clientSocket;
-    private static BufferedReader in;
-    private static PrintWriter out;
+    private static BufferedReader in; //Reader for decoding messages from Server
+    private static PrintWriter out; //PrintWriter for sending messages to the Server
 
 /*Initiates the socket to connect to the server
- * parameters(String argIP, int argPort)
+ *@param argIP IP address to which the client socket tries to connect
+ *@param argPort port to which the client socket connects
 */
     public static void connect(String argIP, int argPort) throws IOException{
         try{
-            clientSocket = new Socket(argIP, argPort);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            clientSocket = new Socket(argIP, argPort); //Creates the client at the desired IP and Port
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //Reads IN messages from Server
+            out = new PrintWriter(clientSocket.getOutputStream(), true);// Sends OUT messages to Server
             System.out.println("Connecting to server at: " + "PORT=" + argPort+ "; IP=" + argIP);
-        } catch (IOException error){ //El catch obtiene los errores cuando no hay conexion con los puertos o con la IP.
+        } catch (IOException error){
             System.out.println("No server found at: " + argIP+", "+ argPort);
             error.getStackTrace();
         }
     }
+
+    /*Sends a message to the Server
+    *@param message string text to send
+    */
     public static void sendmessage(String message) throws IOException{
-        out.println(message);
+        out.println(message); //Sends the message over
         System.out.println("You sent: " + message);
     }
 
+    /*Receives an incoming message from the Server and updates the log*/
     public static void recievemsg() throws IOException{
-        receivedmsg = in.readLine().trim();
+        receivedmsg = in.readLine().trim(); //Gets the received message and trims it to remove any blank spaces.
         System.out.println("Server sent: " + receivedmsg);
-        logicalgi.updatelog(receivedmsg, false);
+        chatGI.updatelog(receivedmsg, false); //Updates the Client message display with the message sent by the Server
     }
-    public static void stop() throws IOException {// method to stop the server if it required
+    
+    /*Stops the connection with the server socket and ends the client socket*/
+    public static void stop() throws IOException {
         in.close();
         out.close();
         clientSocket.close();
